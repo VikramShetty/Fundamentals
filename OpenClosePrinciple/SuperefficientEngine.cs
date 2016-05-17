c8c64103a09408231509d46c843eda77725892b8
Vikram Shetty <vikram@73bit.com>
2016-05-17 10:42:23 +0530

    OCP: Complying to the OCP, when adding a SuperefficientEngine class. We did not touch existing code of class Car or Engine. So Car now is extensible for new Engine without request for modification. This shows us how an object can be extensible without modifying the existing code.

---
+++ OpenClosePrinciple/SuperefficientEngine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosePrinciple
+{
+  public class SuperefficientEngine : Engine
+  {
+    public SuperefficientEngine()
+    {
+    }
+
+    public SuperefficientEngine(Engine otherEngine)
+      : base(otherEngine)
+    {
+    }
+
+    public override Engine makeCopy()
+    {
+      return new SuperefficientEngine(this);
+    }
+  }
+}
