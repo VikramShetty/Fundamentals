88f24e6cb12ff3dcfd703ee095e97765beb10038
Vikram Shetty <vikram@73bit.com>
2017-01-26 16:45:51 +0530

    Encapsulation and Solid Principles Added the initial code with design problem. Design smell like no CQS and no postel's law

---
+++ FundamentalsTests/FileStoreTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SOLID;
+
+namespace FundamentalsTests
+{
+  [TestClass]
+  public class FileStoreTest
+  {
+    private string message = "";
+    [TestMethod]
+    public void WhenRead_With49_ReturnsEmptyString()
+    {
+      FileStore fs = new FileStore();
+      fs.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+      fs.Save(49, "some");
+      fs.MessageRead += new EventHandler<MessageEventArgs>(messageEvent);
+      fs.Read(49);
+      Assert.AreEqual("some", message);
+    }
+
+    void messageEvent(object sender, MessageEventArgs e)
+    {
+      this.message = e.Message;
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void WhenRead_With49_NoWorkingDirectory_ReturnsEmptyString()
+    {
+      FileStore fs = new FileStore();
+      fs.Save(49, "");
+      fs.Read(49);
+      Assert.AreEqual("", "");
+    }
+  }
+}
---
+++ SOLID/FileStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+  public class FileStore
+  {
+    public string WorkingDirectory { get; set; }
+
+    public string Save(int id, string message)
+    {
+      var path = Path.Combine(this.WorkingDirectory, id + ".txt");
+      File.WriteAllText(path, message);
+      return path;
+    }
+
+    public event EventHandler<MessageEventArgs> MessageRead;
+    public void Read(int id)
+    {
+      var path = Path.Combine(this.WorkingDirectory, id + ".txt");
+      var msg = File.ReadAllText(path);
+      this.MessageRead(this, new MessageEventArgs { Message = msg });
+    }
+  }
+}
