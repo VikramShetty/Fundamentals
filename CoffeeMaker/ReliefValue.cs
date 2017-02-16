5862f026203041f6dae4ebdb6fd41dd696803824
Vikram Shetty <vikram@73bit.com>
2017-02-16 11:23:29 +0530

    CoffeeMaker: Added WarmerPlate and ReliefValue class using IObserver

---
+++ CoffeeMaker/ReliefValue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CoffeeMaker
+{
+  public class ReliefValue : IObserver<WarmerPlateStatus>
+  {
+    private readonly ICoffeeMaker hardware;
+
+    public ReliefValue(ICoffeeMaker hardware)
+    {
+      if (hardware == null)
+        throw new ArgumentNullException("hardware");
+
+      this.hardware = hardware;
+    }
+    public void OnNext(WarmerPlateStatus value)
+    {
+      if (value == WarmerPlateStatus.POT_NOT_EMPTY)
+        this.hardware.SetReliefValveState(ReliefValveState.OPEN);
+      else
+        this.hardware.SetReliefValveState(ReliefValveState.CLOSED);
+    }
+
+    public void OnError(Exception error)
+    {
+    }
+
+    public void OnCompleted()
+    {
+    }
+  }
+}
---
+++ CoffeeMaker/WarmerPlate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CoffeeMaker
+{
+  public class WarmerPlate : IObserver<WarmerPlateStatus>
+  {
+    private readonly ICoffeeMaker hardware;
+
+    public WarmerPlate(ICoffeeMaker hardware)
+    {
+      if(hardware == null)
+        throw new ArgumentNullException("hardware");
+
+      this.hardware = hardware;
+    }
+
+    public void OnCompleted()
+    {
+    }
+
+    public void OnError(Exception error)
+    {
+    }
+
+    public void OnNext(WarmerPlateStatus value)
+    {
+      if(value == WarmerPlateStatus.POT_NOT_EMPTY)
+        this.hardware.SetWarmerState(WarmerState.ON);
+      else
+        this.hardware.SetWarmerState(WarmerState.OFF);
+    }
+  }
+}
