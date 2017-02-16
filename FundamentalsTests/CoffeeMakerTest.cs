528c0bd2e04805bbe8ede08a18bd57d36c63a74d
Vikram Shetty <vikram@73bit.com>
2017-02-16 09:59:45 +0530

    Initial CoffeeMaker Empty Project with Hardware API

---
+++ FundamentalsTests/CoffeeMakerTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FundamentalsTests
+{
+  [TestClass]
+  public class CoffeeMakerTest
+  {
+    [TestMethod]
+    public void EmptyTest()
+    {
+      Assert.IsTrue(true);
+    }
+  }
+}
