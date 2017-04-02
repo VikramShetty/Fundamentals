2816bb7d61ce5d7bf0f5cd63a930b401ba0a03b6
Vikram Shetty <vikram@73bit.com>
2017-04-02 12:39:29 +0530

    Adding the project Program to interface. Making use of polymorphic behaviour and seprating the behaviour that can change and does not change much. Also making sure the code reuse happens to its max.

---
+++ FundamentalsTests/ProgramToInterfaceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProgramToInterface;
+
+namespace FundamentalsTests
+{
+  [TestClass]
+  public class ProgramToInterfaceTest
+  {
+    [TestMethod]
+    public void ConcreteClass_BehaviourNotChanging()
+    {
+      Duck duck = new Duck();
+      Assert.AreEqual("All duck can swim!", duck.Swim());
+    }
+  }
+}
---
+++ ProgramToInterface/Duck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramToInterface
+{
+    public class Duck
+    {
+      public string Swim()
+      {
+       return "All duck can swim!";
+      }
+    }
+}
