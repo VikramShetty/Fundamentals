62e270ac992fca4e3fcff2c3fcec1189b609d689
Vikram Shetty <vikram@73bit.com>
2017-04-03 18:34:05 +0530

    Iterator and Composite pattern: Added project. Create Menu Item class the object on which we will work. Created Test class.

---
+++ FundamentalsTests/IteratorAndCompositeTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IteratorAndComposite;
+
+namespace FundamentalsTests
+{
+  [TestClass]
+  public class IteratorAndCompositeTest
+  {
+    [TestMethod]
+    public void GetName_Returns_BLT_MenuItem()
+    {
+      var menuItem = FactorySingleMenuCreator();
+      Assert.AreEqual("BLT", menuItem.GetName());
+    }
+
+    [TestMethod]
+    public void GetDescription_Return_FullText_MenuItem()
+    {
+      var menuItem = FactorySingleMenuCreator();
+      Assert.AreEqual("Bacon with Lettuce & tomato on whole wheat", 
+        menuItem.GetDescription());
+    }
+
+    [TestMethod]
+    public void GetPrice_Returns_2_99_MenuItem()
+    {
+      var menuItem = FactorySingleMenuCreator();
+      Assert.AreEqual(2.99, menuItem.GetPrice());
+    }
+
+    [TestMethod]
+    public void IsVegetarian_Returns_False_MenuItem()
+    {
+      var menuItem = FactorySingleMenuCreator();
+      Assert.AreEqual(false, menuItem.IsVegetarian());
+    }
+    
+    private MenuItem FactorySingleMenuCreator()
+    {
+      return new MenuItem("BLT", "Bacon with Lettuce & tomato on whole wheat", false, 2.99);
+    }
+  }
+}
---
+++ IteratorAndComposite/MenuItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorAndComposite
+{
+    public class MenuItem
+    {
+      private string Name;
+      private string Description;
+      private bool Vegetarian;
+      private double Price;
+
+      public MenuItem(string Name, string Description, bool Vegetarian, double price)
+      {
+        this.Name = Name;
+        this.Description = Description;
+        this.Vegetarian = Vegetarian;
+        this.Price = price;
+      }
+
+      public string GetName()
+      {
+        return this.Name;
+      }
+
+      public string GetDescription()
+      {
+        return this.Description;
+      }
+
+      public double GetPrice()
+      {
+        return this.Price;
+      }
+
+      public bool IsVegetarian()
+      {
+        return this.Vegetarian;
+      }
+    }
+}
