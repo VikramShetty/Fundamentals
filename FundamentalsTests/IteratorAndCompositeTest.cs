894fa7d1c51b328c6768ba372885931ab2023b23
Vikram Shetty <vikram@73bit.com>
2017-04-05 18:48:48 +0530

    Removed all the Iterator of menus and changed it to MenuCompoment. Now adding more menus is easy.

--- FundamentalsTests/IteratorAndCompositeTest.cs
+++ FundamentalsTests/IteratorAndCompositeTest.cs
@@ -93,128 +93,14 @@ namespace FundamentalsTests
     }
     #endregion
     #region "PanCakeHouseMenu"
-
-    [TestMethod]
-    public void MoreThanZero_Item_InMenu_Of_PanCakeHouseMenu()
-    {
-      //Using IEnumerator because of this http://www.c-sharpcorner.com/UploadFile/219d4d/ienumerable-vs-ienumerator-in-C-Sharp/
-      //Removed because internals of the object should not be access directly
-      //var panCakeHouseMenu = new PanCakeHouseMenu();
-      //IEnumerator panCakeHouseIterator = panCakeHouseMenu.createIterator();
-      //Assert.AreEqual(true, panCakeHouseIterator.Count() > 0);
-    }
-
-    [TestMethod]
-    public void Four_Items_InMenu_Of_PanCakeHouseMenu()
-    {
-      //Using IEnumerator because of this http://www.c-sharpcorner.com/UploadFile/219d4d/ienumerable-vs-ienumerator-in-C-Sharp/
-      //Removed because internals of the object should not be access directly
-      //var panCakeHouseMenu = new PanCakeHouseMenu();
-      //IEnumerator panCakeHouseIterator = panCakeHouseMenu.createIterator();
-      //Assert.AreEqual(4, panCakeHouseIterator.Count());
-    }
-
-    [TestMethod]
-    public void KNB_Pancake_IsFirstMenu_In_PanCakeHouseMenu()
-    {
-      //Removed because internals of the object should not be access directly
-      //var panCakeHouseMenu = new PanCakeHouseMenu();
-      //Assert.AreEqual(CONST.P_1_NAME, panCakeHouseMenu.getMenuItems()[0].GetName());
-    }
-
-    [TestMethod]
-    public void T_2_99_IsCost_Of_FirstMenu_In_PanCakeHouseMenu()
-    {
-      //Removed because internals of the object should not be access directly
-      //var panCakeHouseMenu = new PanCakeHouseMenu();
-      //Assert.AreEqual(CONST.P_1_PRICE, panCakeHouseMenu.getMenuItems()[0].GetPrice());
-    }
-
-    [TestMethod]
-    public void FirstMenu_Is_Veg_In_PanCakeHouseMenu()
-    {
-      //Removed because internals of the object should not be access directly
-      //var panCakeHouseMenu = new PanCakeHouseMenu();
-      //Assert.AreEqual(CONST.P_1_VEG, panCakeHouseMenu.getMenuItems()[0].IsVegetarian());
-    }
-
-    [TestMethod]
-    public void Pancake_IsSecondMenu_In_PanCakeHouseMenu()
-    {
-      //Removed because internals of the object should not be access directly
-      //var panCakeHouseMenu = new PanCakeHouseMenu();
-      //Assert.AreEqual(CONST.P_2_NAME, panCakeHouseMenu.getMenuItems()[1].GetName());
-    }
-
-    [TestMethod]
-    public void BlueberryPancake_IsSecondMenu_In_PanCakeHouseMenu()
-    {
-      //Removed because internals of the object should not be access directly
-      //var panCakeHouseMenu = new PanCakeHouseMenu();
-      //Assert.AreEqual(CONST.P_3_NAME, panCakeHouseMenu.getMenuItems()[2].GetName());
-    }
-
-    [TestMethod]
-    public void WafflesPancake_IsSecondMenu_In_PanCakeHouseMenu()
-    {
-      //Removed because internals of the object should not be access directly
-      //var panCakeHouseMenu = new PanCakeHouseMenu();
-      //Assert.AreEqual(CONST.P_4_NAME, panCakeHouseMenu.getMenuItems()[3].GetName());
-    }
-
+    //Using IEnumerator because of this http://www.c-sharpcorner.com/UploadFile/219d4d/ienumerable-vs-ienumerator-in-C-Sharp/
+    //Removed because internals of the object should not be access directly
+    //Removed all tests under this
     #endregion
     #region "DinnerMenu"
-    [TestMethod]
-    public void MoreThanZero_Item_InMenu_Of_DinnerMenu()
-    {
-      //Using IEnumerator because of this http://www.c-sharpcorner.com/UploadFile/219d4d/ienumerable-vs-ienumerator-in-C-Sharp/
-      //Removed because internals of the object should not be access directly
-      //var dinnerMenu = new DinnerMenu();
-      //Iterator dinnerIterator = dinnerMenu.createIterator();
-      //Assert.AreEqual(true, dinnerIterator.Count() > 0);
-    }
-
-    [TestMethod]
-    public void Six_Items_InMenu_Of_DinnerMenu()
-    {
-      //Using IEnumerator because of this http://www.c-sharpcorner.com/UploadFile/219d4d/ienumerable-vs-ienumerator-in-C-Sharp/
-      //Removed because internals of the object should not be access directly
-      //var dinnerMenu = new DinnerMenu();
-      //Iterator dinnerIterator = dinnerMenu.createIterator();
-      //Assert.AreEqual(6, dinnerIterator.Count());
-    }
-
-    [TestMethod]
-    public void VegBLT_IsFirstMenu_In_DinnerMenu()
-    {
-      //Removed because internals of the object should not be access directly
-      //var dinnerMenu = new DinnerMenu();
-      //Assert.AreEqual(CONST.D_1_NAME, dinnerMenu.getMenuItems()[0].GetName());
-    }
-
-    [TestMethod]
-    public void HotDog_IsLastMenu_In_DinnerMenu()
-    {
-      //Removed because internals of the object should not be access directly
-      //var dinnerMenu = new DinnerMenu();
-      //Assert.AreEqual(CONST.D_4_NAME, dinnerMenu.getMenuItems()[3].GetName());
-    }
-
-    [TestMethod]
-    public void Soup_IsThirdMenu_In_DinnerMenu()
-    {
-      //Removed because internals of the object should not be access directly
-      //var dinnerMenu = new DinnerMenu();
-      //Assert.AreEqual(CONST.D_3_NAME, dinnerMenu.getMenuItems()[2].GetName());
-    }
-
-    [TestMethod]
-    public void BLT_IsSecondMenu_In_DinnerMenu()
-    {
-      //Removed because internals of the object should not be access directly
-      //var dinnerMenu = new DinnerMenu();
-      //Assert.AreEqual(CONST.D_2_NAME, dinnerMenu.getMenuItems()[1].GetName());
-    }
+    //Using IEnumerator because of this http://www.c-sharpcorner.com/UploadFile/219d4d/ienumerable-vs-ienumerator-in-C-Sharp/
+    //Removed because internals of the object should not be access directly
+    //Removed all tests under this
     #endregion
     #region "Waitress"
     [TestMethod]
--- IteratorAndComposite/Waitress.cs
+++ IteratorAndComposite/Waitress.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-
-namespace IteratorAndComposite
+﻿namespace IteratorAndComposite
 {
   public class Waitress
   {
